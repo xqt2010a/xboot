e5842a6e441a2e18d441c206aea3c99e22b387d9
jiangjianjun <8192542@qq.com>
2018-12-12 15:08:02 +0800

    [graphic]working on graphic

---
+++ src/include/graphic/display.h
@@ -0,0 +1,19 @@
+#ifndef __GRAPHIC_DISPLAY_H__
+#define __GRAPHIC_DISPLAY_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <xboot.h>
+
+struct display_t
+{
+	void * priv;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GRAPHIC_DISPLAY_H__ */
